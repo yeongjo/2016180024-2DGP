d4481009855e12a32a0c04d5003915859f36ff5d
yeongjo_school <songchung2466@gmail.com>
2019-10-02 19:04:51 +0900

    Drill 04 완성
    drill 04 2016180024 송영조
    https://youtu.be/5QaZ-ObnzOk

--- Drill-04/move_character_with_key.py
+++ Drill-04/move_character_with_key.py
@@ -1,51 +1,29 @@
 from pico2d import *
 
-
-def handle_events():
-    global running
-    global dir
-    events = get_events()
-
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            print('key down')
-            if event.key == SDLK_RIGHT:
-                dir += 1
-            elif event.key == SDLK_LEFT:
-                dir -= 1
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-            elif event.key == SDLK_LEFT:
-                dir += 1
-    pass
-
-
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 running = True
-dir = 0
+dir = 1
+img_dir = 1
 x = 800 // 2
 frame = 0
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    for y in range(1, 1000):
-        character.clip_draw(frame * 100, 100 * 1, 100, 80, x, y)
+    character.clip_draw(frame * 100, 100 * img_dir, 100, 100, x, 90)
     update_canvas()
-
-    handle_events()
     frame = (frame + 1) % 8
-    x += dir * 5
-    print(dir)
-    #delay(0.03)
+    x += dir * 15
+    if x >= 800:
+        dir = -1
+        img_dir = 0
+    elif x <= 0:
+        dir = 1
+        img_dir = 1
+    delay(0.03)
 
 close_canvas()
 
