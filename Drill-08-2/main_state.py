11f6efeed3430eb71b5fe0b3e033ea6099dca94f
songch <songchung2466@gmail.com>
2019-10-21 20:20:33 +0900

    Drill 08-2 완성 2016180024 송영조 커밋횟수 2회 https://youtu.be/TIrxQ8KoBrU

---
+++ Drill-08-2/main_state.py
@@ -0,0 +1,88 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+import pause_state
+
+
+
+name = "MainState"
+
+boy = None
+grass = None
+font = None
+
+
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+        self.dir = 1
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += self.dir
+        if self.x >= 800:
+            self.dir = -1
+        elif self.x <= 0:
+            self.dir = 1
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+def enter():
+    global boy, grass
+    boy = Boy()
+    grass = Grass()
+
+
+def exit():
+    global boy, grass
+    del(boy)
+    del(grass)
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for e in events:
+        if e.type == SDL_QUIT:
+            game_framework.quit()
+        elif e.type == SDL_KEYDOWN and e.key == SDLK_p:
+            game_framework.push_state(pause_state)
+
+
+def update():
+    boy.update()
+
+
+def draw():
+    grass.draw()
+    boy.draw()
+
+
+
+
+
