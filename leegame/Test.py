913259b542e87aab32b8a3acdb38fad73e0992e2
sc2466 <songchung2466@gmail.com>
2019-11-05 02:16:32 +0900

    불필요한것들 제거

--- leegame/Test.py
+++
@@ -1,4 +0,0 @@
-def open_canvas():
-    global canvas_width
-    global canvas_height
-    canvas_width, canvas_height = 800, 600
--- leegame/Test2.py
+++
@@ -1,6 +0,0 @@
-import pico2d as tt
-
-tt.open_canvas()
-print(tt.get_canvas_height())
-tt.canvas_height = 10
-print(tt.get_canvas_height())
