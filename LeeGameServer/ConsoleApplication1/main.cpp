c53ccd2d2a75080f0668983bccb65b2c16e36054
Yum <yumtr@naver.com>
2020-11-20 03:11:20 +0900

    create server socket & recv packet

--- LeeGameServer/ConsoleApplication1/main.cpp
+++ LeeGameServer/ConsoleApplication1/main.cpp
@@ -4,9 +4,63 @@
 #include "GameManager.h"
 #include "Packets.h"
 
+DWORD WINAPI ProcessClient(LPVOID arg)
+{
+	SOCKET client_sock = (SOCKET)arg;
+	unsigned int count;
+	int retval;
+	SOCKADDR_IN clientaddr;
+	int addrlen = sizeof(clientaddr);
+	char buf[BUFSIZE];	
+
+	// 클라이언트 정보 얻기    
+	getpeername(client_sock, (SOCKADDR*)&clientaddr, &addrlen);	
+			
+	retval = recv(client_sock, buf, BUFSIZE, 0);
+	if (retval == SOCKET_ERROR) { err_display("recv() err 3"); }	
+
+	char* tok2 = strtok(buf, "}");
+	strcat(tok2, "}");
+	cout << tok2 << endl;
+	
+
+	closesocket(client_sock);
+	printf("\n[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트번호=%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+	return 0;
+}
+
 int main()
 {
-	GameManager gm;
+	GameManager gm;	
+	int retval;
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	//socket
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_quit("socket() err");
+
+	//bind
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind() err");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	vector<HANDLE> hThread;
+
 
 	// 네트워크 접속 관련 추가되어야하는곳
 	//...
@@ -14,7 +68,21 @@ int main()
 	int playerCnt = 0; // 들어온 플레이어 수도 알려줘
 	// 플레이어 들어옴
 	// 클라이언트에게 playerCnt를 ID로 전송
-	playerCnt++;
+	
+	while (1)
+	{
+		//accept        
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET) { err_display("accept() err"); break; }
+
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트번호=%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+		// 스레드 생성        		
+		hThread.push_back(CreateThread(NULL, 0, ProcessClient, (LPVOID)client_sock, 0, NULL));
+		if (hThread[playerCnt] == NULL) { closesocket(client_sock); }
+		else { CloseHandle(hThread[playerCnt]); }
+		playerCnt++;
+	}
 
 
 	// 네트워크에 접속되고 나서 호출되어야함
@@ -24,8 +92,9 @@ int main()
 		gm.Update(0.016f);
 		Sleep(16);
 	}
+	
 
-	//c++ json 사용 예
+	////c++ json 사용 예
 	//MapDataPacket w;
 	//w.furniturePos.push_back({ 1, 0 });
 	//w.furniturePos.push_back({ 12, 03 });
@@ -36,4 +105,8 @@ int main()
 	//MapDataPacket w2;
 	//CJsonSerializer::Deserialize(&w2, s);
 	//cout << endl;
+	//cout << w2.furniturePos[0][0] <<" "  << w2.furniturePos[0][1] << endl;	//1  0
+	//cout << w2.furniturePos[1][0] << " " << w2.furniturePos[1][1] << endl;	//12 3
+	closesocket(listen_sock);
+	WSACleanup();
 }
--- LeeGameServer/ConsoleApplication1/stdafx.h
+++ LeeGameServer/ConsoleApplication1/stdafx.h
@@ -1,11 +1,17 @@
 #pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(lib, "ws2_32")
 #include <iostream>
 #include <json/json.h>
 #include <sstream>
 #include "glm/vec2.hpp"
 #include <vector>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <windows.h>
 #include <tchar.h>
+
 
 using namespace std;
 using namespace glm;
@@ -22,3 +28,32 @@ inline float Random() {
 void print(LPCTSTR pszStr, ...);
 
 #define DebugPrint(...) print(L"%s %s %d: ", __FILEW__, __FUNCTIONW__, __LINE__, __VA_ARGS__);
+
+
+#define SERVERIP    "127.0.0.1"
+#define SERVERPORT  9000
+#define BUFSIZE     200
+#define MAXPLAYER   2
+
+inline void err_quit(const char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+    MessageBox(NULL, (LPCTSTR)lpMsgBuf, (LPCWSTR)msg, MB_ICONERROR);
+    LocalFree(lpMsgBuf);
+    exit(1);
+}
+
+inline void err_display(const char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+    printf("[%s] %s", msg, (char*)lpMsgBuf);
+    LocalFree(lpMsgBuf);
+}
